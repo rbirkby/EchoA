ac3781e7d8907f5711a2320083eb70e3213b121e
Richard Birkby <rbirkby@gmail.com>
2012-06-08 14:52:29 +0100

    First code commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace ThunderMain.Text
+{
+	static class EchoA
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 0 && args[0].Length == 5 && args[0].StartsWith("/A:"))
+			{
+				string hexDigits = "abcdef0123456789";
+
+				if (hexDigits.Contains(char.ToLower(args[0][3])) && hexDigits.Contains(char.ToLower(args[0][4])))
+				{
+					Console.BackgroundColor = (ConsoleColor)int.Parse(args[0].Substring(3, 1), NumberStyles.HexNumber);
+					Console.ForegroundColor = (ConsoleColor)int.Parse(args[0].Substring(4, 1), NumberStyles.HexNumber);
+					args = args.Skip(1).ToArray();
+				}
+			}
+
+			Console.Write(string.Join(" ", args));
+			Console.ResetColor();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,28 @@ EchoA
 =====
 
 ANSI color Echo alternative for batch scripts
+
+## Examples
+
+    EchoA /A:0b Hello World
+    EchoA /A:0b Info && EchoA /A:2f Commands available && Echo.
+
+You can also use EchoA without default colors:
+
+    EchoA Hello World
+
+## ANSI color codes
+
+Just type color /? at a DOS prompt to see the available codes.
+For reference, they are:
+
+    0 = Black       8 = Gray
+    1 = Blue        9 = Light Blue
+    2 = Green       A = Light Green
+    3 = Aqua        B = Light Aqua
+    4 = Red         C = Light Red
+    5 = Purple      D = Light Purple
+    6 = Yellow      E = Light Yellow
+    7 = White       F = Bright White
+
+    
